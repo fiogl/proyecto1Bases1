27581e47720f9372faff2fc2bd4e48f74c4e1bfa
flori <flrodriguez@estudiantec.cr>
2025-04-20 20:03:29 -0600

    Ya funciona el guardado de observaciones, falta que borre y que se puedan hacer las identificaciones (enseñar los detalles de las observaciones)

--- proyecto1/src/main/java/com/postgresql/proyecto1/controller/HomeController.java
+++ proyecto1/src/main/java/com/postgresql/proyecto1/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.postgresql.proyecto1.controller;
 
 import com.postgresql.proyecto1.model.Image;
 import com.postgresql.proyecto1.model.Observation;
+import com.postgresql.proyecto1.model.User;
 import com.postgresql.proyecto1.repo.ObservationRepo;
+import com.postgresql.proyecto1.repo.UserRepo;
 import com.postgresql.proyecto1.repo.TaxonRepo;
 import com.postgresql.proyecto1.repo.LicenseRepo;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ public class HomeController {
     private final ObservationRepo observationRepo;
     private final TaxonRepo taxonRepo;
     private final LicenseRepo licenseRepo;
+    private final UserRepo userRepo;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -26,10 +29,8 @@ public class HomeController {
     @GetMapping("/create")
     public String showCreateForm(Model model) {
         Observation obs = new Observation();
-        obs.setImage(new Image());
+
         model.addAttribute("observation", obs);
-
-
         model.addAttribute("taxon", taxonRepo.findAll());
         model.addAttribute("license", licenseRepo.findAll());
         return "create";
@@ -37,6 +38,13 @@ public class HomeController {
 
     @PostMapping("/create")
     public String createObservation(@ModelAttribute Observation observation) {
+        User user = userRepo.findByEmail("florita2901@gmail.com");
+        observation.setUser(user);
+
+        if (observation.getImage() != null) {
+            observation.getImage().setUser(user);
+        }
+
         observationRepo.save(observation);
         return "redirect:/";
     }
--- proyecto1/src/main/java/com/postgresql/proyecto1/repo/UserRepo.java
+++ proyecto1/src/main/java/com/postgresql/proyecto1/repo/UserRepo.java
@@ -6,4 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface UserRepo extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
