dd94b2b81bfed264e87d9d99219d46f5897fa3f9
fiogl <fiorellagonzalezlopez28@gmail.com>
2025-04-27 00:06:56 -0600

    Consulta de taxones (falta funcionamiento propio de reino y especie :P)

--- proyecto1/src/main/java/com/postgresql/proyecto1/repo/TaxonRepo.java
+++ proyecto1/src/main/java/com/postgresql/proyecto1/repo/TaxonRepo.java
@@ -2,6 +2,8 @@ package com.postgresql.proyecto1.repo;
 
 import com.postgresql.proyecto1.model.Taxon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -9,5 +11,7 @@ import java.util.List;
 @RepositoryRestResource
 public interface TaxonRepo extends JpaRepository<Taxon, Integer> {
     // Para buscar si otros taxones decienden del actual
-    List<Taxon> findByAncestor(Taxon ancestor);
+    @Query("SELECT t FROM Taxon t WHERE t.ancestor = :ancestor")
+    List<Taxon> findByAncestor(@Param("ancestor") Taxon ancestor);
+
 }
