b2e634a652b07db73bc08cdb52bb69ca2ddbae07
flori <flrodriguez@estudiantec.cr>
2025-05-01 16:28:54 -0600

    Botón de atrás en make consults (taxon observations)

--- proyecto1/src/main/java/com/postgresql/proyecto1/repo/ObservationRepo.java
+++ proyecto1/src/main/java/com/postgresql/proyecto1/repo/ObservationRepo.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-import java.util.Optional;
 
 @RepositoryRestResource
 public interface ObservationRepo extends JpaRepository<Observation, Integer> {
