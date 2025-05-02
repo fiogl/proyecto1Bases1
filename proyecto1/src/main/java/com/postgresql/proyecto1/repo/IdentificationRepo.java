5d79542ce9e2f9edbe9a42db29f294c4b04c2f51
fiogl <fiorellagonzalezlopez28@gmail.com>
2025-05-02 13:22:17 -0600

    Consulta identificaciones

--- proyecto1/src/main/java/com/postgresql/proyecto1/repo/IdentificationRepo.java
+++ proyecto1/src/main/java/com/postgresql/proyecto1/repo/IdentificationRepo.java
@@ -2,8 +2,31 @@ package com.postgresql.proyecto1.repo;
 
 import com.postgresql.proyecto1.model.Identification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface IdentificationRepo extends JpaRepository<Identification, Integer> {
+    @Query(value = """
+    SELECT t.common_name, COUNT(DISTINCT i.id_identification), COUNT(DISTINCT ui.id_user)
+    FROM identification i
+    INNER JOIN observation o 
+    
+    ON i.observation_id = o.id_observation
+            
+    INNER JOIN taxon t 
+    ON i.taxon_id = t.id_taxon
+    
+    INNER JOIN users ui 
+    ON i.user_id = ui.id_user
+            
+    INNER JOIN users uo 
+    ON o.user_id = uo.id_user
+    
+    GROUP BY t.common_name
+    ORDER BY COUNT(DISTINCT i.id_identification) DESC
+    """, nativeQuery = true)
+    List<Object[]> getConsult();
 }
